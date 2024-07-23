706565c26ba8d326bea9aebc5706616a7cd69bf0
IT22079572 <it22079572@my.sliit.lk>
2024-07-23 15:42:38 +0530

    password manager

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,13 +1,23 @@
-pwd = input("What is the master passwrod : ")
+mst_pwd = input("What is the master password: ")
 
 def view():
-    pass
+    try:
+        with open('password.txt', 'r') as f:
+            for line in f.readlines():
+                name, pwd = line.strip().split()
+                print(f"Account: {name}, Password: {pwd}")
+    except FileNotFoundError:
+        print("No passwords found. Please add some first.")
 
 def add():
-    pass
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+    
+    with open('password.txt', 'a') as f:
+        f.write(name + " " + pwd + "\n")
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones : ")
+    mode = input("Would you like to add a new password or view existing ones (add/view), or press 'q' to quit: ").lower()
     
     if mode == "q":
         break
@@ -17,5 +27,4 @@ while True:
     elif mode == "add":
         add()
     else:
-       print("Invalid Mode...!") 
-       continue
+        print("Invalid mode. Please try again.")
